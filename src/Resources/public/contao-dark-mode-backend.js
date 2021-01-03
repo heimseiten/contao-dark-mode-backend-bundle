a3708cbdad43d8f2b89e7d53b8f704cf5a66d882
Niels <info@heimseiten.de>
2021-01-03 19:03:25 +0100

    Update contao-dark-mode-backend.js
    added tooltip

--- src/Resources/public/contao-dark-mode-backend.js
+++ src/Resources/public/contao-dark-mode-backend.js
@@ -8,10 +8,16 @@ document.addEventListener( 'DOMContentLoaded', function () {
     
     var newEntry = document.createElement('li')
     newEntry.setAttribute('class','dark_mode_toggle')
-    newEntry.innerHTML = '<a>Dark Mode</a>'
+    newEntry.innerHTML = '<a title="Dark Mode on/off">Dark Mode</a>'
     
     document.querySelector('#tmenu li:first-child').before(newEntry)
     
+    new Tips.Contao($$('#tmenu a[title]').filter(function(i) {
+        return i.title != '';
+    }), {
+        offset: {x:9, y:42}
+    })
+
     document.querySelector('.dark_mode_toggle').addEventListener('click', event => {      
         if ( document.querySelector('body').classList.contains('dark_mode_off') ) {
             document.querySelector('body').classList.remove('dark_mode_off')
