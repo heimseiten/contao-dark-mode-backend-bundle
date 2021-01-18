b92786ce89fa36407bbf6637d42137ea49a58b93
Niels <info@heimseiten.de>
2021-01-18 10:16:06 +0100

    Update contao-dark-mode-backend.js

--- src/Resources/public/contao-dark-mode-backend.js
+++ src/Resources/public/contao-dark-mode-backend.js
@@ -10,7 +10,9 @@ document.addEventListener( 'DOMContentLoaded', function () {
     newEntry.setAttribute('class','dark_mode_toggle')
     newEntry.innerHTML = '<a title="Dark Mode on/off">Dark Mode</a>'
     
-    document.querySelector('#tmenu li:first-child').before(newEntry)
+    if (document.querySelector('#tmenu li:first-child')) {
+        document.querySelector('#tmenu li:first-child').before(newEntry)
+    }
     
     new Tips.Contao($$('#tmenu a[title]').filter(function(i) {
         return i.title != '';
@@ -18,14 +20,16 @@ document.addEventListener( 'DOMContentLoaded', function () {
         offset: {x:9, y:42}
     })
 
-    document.querySelector('.dark_mode_toggle').addEventListener('click', event => {      
-        if ( document.querySelector('body').classList.contains('dark_mode_off') ) {
-            document.querySelector('body').classList.remove('dark_mode_off')
-            localStorage.setItem('dark_mode_off', 'false')
-        } else {
-            document.querySelector('body').classList.add('dark_mode_off')
-            localStorage.setItem('dark_mode_off', 'true')
-        }
-    })
+    if (document.querySelector('.dark_mode_toggle')) {
+        document.querySelector('.dark_mode_toggle').addEventListener('click', event => {      
+            if ( document.querySelector('body').classList.contains('dark_mode_off') ) {
+                document.querySelector('body').classList.remove('dark_mode_off')
+                localStorage.setItem('dark_mode_off', 'false')
+            } else {
+                document.querySelector('body').classList.add('dark_mode_off')
+                localStorage.setItem('dark_mode_off', 'true')
+            }
+        })
+    }
 
 });
