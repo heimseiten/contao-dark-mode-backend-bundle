3740f5528dcad579ccdd16ea8954d80bfb785daa
Niels <info@heimseiten.de>
2022-08-19 13:13:49 +0200

    Update config.php

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -11,7 +11,10 @@
  *
  */
 
-if (\defined('TL_MODE') && 'BE' === TL_MODE) {
+use Contao\System;
+use Symfony\Component\HttpFoundation\Request;
+
+if (System::getContainer()->get('contao.routing.scope_matcher')->isBackendRequest(System::getContainer()->get('request_stack')->getCurrentRequest() ?? Request::create(''))) {
     $GLOBALS['TL_CSS'][] = 'bundles/heimseitencontaodarkmodebackend/contao-dark-mode-backend.scss|static';
     $GLOBALS['TL_JAVASCRIPT'][] = 'bundles/heimseitencontaodarkmodebackend/contao-dark-mode-backend.js';
 }
